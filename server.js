b3a63e0c7b0021d823a2a05790e57ff7ce7cfe08
levids <lyam567@hotmail.co.uk>
2022-03-07 11:45:52 +0000

    added css to shopping list, changed nav bar to partial view and moved its styling to styles.css

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB()
 
 
 app.set('view engine', 'ejs')
+app.set('views','./views')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
