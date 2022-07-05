8b2bf45b063ff154b662c9aafd3d675835be6c89
Lyam Walburn <lyam567@hotmail.co.uk>
2022-07-05 15:37:55 +0100

    reconfigured how ingredients are held while creating a new recipie, fixed deleting

--- public/js/addMeal.js
+++ public/js/addMeal.js
@@ -8,8 +8,8 @@ let imagesPaths
 const instructionInput = document.querySelector('#instructionInput')
 const addInstructionButton = document.querySelector('.addInstructionButton').addEventListener('click',addInstructionStep)
 
-const ingredients = []
-const instructions = []
+let ingredients = {}
+let instructions = []
 
 const submitButton = document.querySelector('#submitButton').addEventListener('click',createRecipe)
 
@@ -20,34 +20,46 @@ fetch('http://localhost:3000/recipes/getImagePaths')
 function addIngredient(){
     const itemUnit = document.querySelector('#ingredientUnit')
     const ingredientsList = document.querySelector('.ingredientsList')
-    const ingredientTA = document.querySelector('#ingredientsTA')
 
+    if(!ingredients[`${itemName.value}`]){
+        ingredients[`${itemName.value}`] = {name : itemName.value,ammount : itemAmmount.value, unit: itemUnit.value, image: `${dirPath}${itemName.value.replace(' ','-')}.jpeg`}
+        const li = createIngredientLi(itemName.value,itemAmmount.value,itemUnit.value)
+        ingredientsList.appendChild(li)
+    }
+    else{
+        ingredients[`${itemName.value}`].ammount = parseInt(ingredients[`${itemName.value}`].ammount) + parseInt(itemAmmount.value)
+        //find and update existing li text
+        const li = document.querySelector(`[data-value="${itemName.value}"]`)
+        li.childNodes[1].data = `${ingredients[`${itemName.value}`].ammount}${itemUnit.value} ${itemName.value}`
+    }
+
+    itemName.value = ""
+    itemAmmount.value = ""
+    itemUnit.value = ""
+    itemName.focus()
+}
+
+function createIngredientLi(name,ammount,unit){
     const li = document.createElement('li')
-    // const button = document.createElement('button')
-    // button.innerHTML = "Delete"
-    // button.classList.add('deleteButton')
+    const button = document.createElement('button')
+    button.innerHTML = "Delete"
+    button.classList.add('deleteButton')
+    button.type = "button"
+    button.addEventListener('click',deleteIngredient)
     const pic = document.createElement('picture')
     const image = document.createElement('img')
-    image.src = `${dirPath}${itemName.value.replace(' ','-')}.jpeg`
+    image.src = `${dirPath}${name.replace(' ','-')}.jpeg`
     image.width = 40
     image.height = 40
     image.loading = 'lazy'
     image.decoding = 'async'
     pic.appendChild(image)
     li.appendChild(pic)
-    li.appendChild(document.createTextNode(`${itemAmmount.value}${itemUnit.value} ${itemName.value}`))
-    
+    li.appendChild(document.createTextNode(`${ammount}${unit} ${name}`))
+    li.appendChild(button)
+    li.dataset.value = `${name}`
 
-    // li.appendChild(button)
-    ingredientsList.appendChild(li)
-
-    //add ingredients array to be used for submission -- '\n' will need to be added when combining the string for submission 
-    ingredients.push(`${itemName.value},${itemAmmount.value},${itemUnit.value},${dirPath}${itemName.value.replace(' ','-')}.jpeg`)
-
-    itemName.value = ""
-    itemAmmount.value = ""
-    itemUnit.value = ""
-    itemName.focus()
+    return li
 }
 
 itemName.onkeyup = (e)=> {
@@ -92,9 +104,7 @@ function showSuggestions(list){
 }
 
 function addInstructionStep(){
-    const instructionsTA = document.querySelector('#instructionsTA')
     const instructionsList = document.querySelector('.instructionsList')
-
     const li = document.createElement('li')
     //split the input and feed into paragraphs to maintain spacing on display
     console.log(buildInstructionStepString(instructionInput.value.split('\n')))
@@ -106,6 +116,13 @@ function addInstructionStep(){
        element.appendChild(document.createTextNode(p))
        li.appendChild(element)
     })
+    //create delete button
+    const button = document.createElement('button')
+    button.innerHTML = "Delete"
+    button.classList.add('deleteButton')
+    button.type = "button"
+    button.addEventListener('click',deleteInstruction)
+    li.appendChild(button)
     instructionsList.appendChild(li)
 
     //add instructions array to be used for submission -- '\n' will need to be added after elements when combining the string for submission 
@@ -128,13 +145,39 @@ function buildInstructionStepString(input){
     return output.trim()
 }
 
+function deleteIngredient(){
+    //get the ingredient name from li
+    let item = this.parentElement.dataset.value
+
+    //remove the ingredient from the ingredients object
+    delete ingredients[item]
+
+    //remove li from page
+    let lis = document.querySelectorAll(`[data-value="${item}"]`)
+    lis.forEach(li => {
+        li.remove()
+   })
+}
+
+
+function deleteInstruction(){
+    //TODO requires fix from deleteIngredient
+}
+
 async function createRecipe(){
 
     //TODO - check we have at least 1 ingredient
     //     - check we have at least 1 instruction
     //     - check we have selected / uploaded a main image for meal
 
+    //Object.keys(tempIngredients).length - measures length of objects inside object
+    if(!Object.keys(tempIngredients).length > 0 && !instructions.length > 0) return  //TODO -display error messages
 
+    //format the ingredients object to be sent to the server
+    let ingredientsOutput = []
+    for(let item in ingredients){
+        ingredientsOutput.push(`${ingredients[item].name},${ingredients[item].ammount},${ingredients[item].unit},${ingredients[item].image}`)
+    }
     try{
         const response = await fetch('/recipes/createRecipe', {
             method: 'post',
@@ -144,11 +187,14 @@ async function createRecipe(){
                 "style": document.querySelector('#recipeStyle').value,
                 "cookingTime": document.querySelector('#cookingTime').value,
                 "image": document.querySelector('#recipieImage').value,
-                "ingredients": ingredients.join('\r\n'),
+                "ingredients": ingredientsOutput.join('\r\n'),  
                 "instructions": instructions.join('\r\n')
             })
         })
-        location.reload()
+        .then(data => {
+            //redirect back to recipies page with success message - TODO
+            window.location.replace(data.url)
+        })
     }catch(err){
         console.log(err)
     }
