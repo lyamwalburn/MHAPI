f48f49088de5d55d795cd4da95054b7824bdcc3c
Lyam Walburn <lyam567@hotmail.co.uk>
2022-08-22 10:46:42 +0100

    added config

---
+++ config/config.js
@@ -0,0 +1,6 @@
+
+
+exports.destroySessionUrl = 'https://meal-helper-heroku.herokuapp.com/';
+exports.useMongoDBSessionStore = false;
+exports.databaseUri = 'mongodb://localhost/OIDCStrategy';
+exports.mongoDBSessionMaxAge = 24 * 60 * 60;
